1317718f764e4ac5ecbb976a7f9339a90944182a
Mehdin Hodzic <mehdinh@itineris.ba>
2016-07-01 12:24:36 +0200

    Added base projects for sample application

---
+++ Demos/SampleApplication.Domain/Documents/IDocumentRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApplication.Domain.Documents
+{
+    public interface IDocumentRepository
+    {
+        Document GetById(int id);
+
+        Document GetByName(string name);
+
+        IList<Document> GetAll();
+    }
+}
