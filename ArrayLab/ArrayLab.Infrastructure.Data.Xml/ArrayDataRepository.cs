d06475434cff51679e56659027393ea429ed7482
Mehdin Hodzic <mehdinh@itineris.ba>
2016-07-27 11:29:59 +0200

    Updated code regarding ArrayLab project (project that will use extensions / plugins in order to add functionality to work with arrays)

---
+++ ArrayLab/ArrayLab.Infrastructure.Data.Xml/ArrayDataRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayLab.Infrastructure.Data.Xml
+{
+    public class ArrayDataRepository
+    {
+    }
+}
