d19d742522f6161c87d0ff6e4762b47a38232554
Mehdin Hodzic <mehdinh@itineris.ba>
2016-09-01 15:37:00 +0200

    Added new project for document classes

---
+++ Other/CodeBlocks/Repository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlocks
+{
+    public class Repository
+    {
+        public Repository(ILogger logger)
+        {
+            this.logger = logger;
+        }
+
+        private ILogger logger;        
+    }
+}
---
+++ Other/CodeBlocks/RepositoryConsumer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlocks
+{
+    public class RepositoryConsumer
+    {
+        public void Consume()
+        {
+            ILogger logger = new ConsoleLogger();
+            Repository repository = new Repository(logger);
+        }
+    }
+}
