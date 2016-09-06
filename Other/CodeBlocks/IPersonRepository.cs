8aec85257f36012a8c8ef68c55917890590cc7f3
Mehdin Hodzic <mehdinh@outlook.com>
2016-09-07 01:17:42 +0200

    Updates on project used for code samples in the document.

---
+++ Other/CodeBlocks/IPersonRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlocks
+{
+    public interface IPersonRepository
+    {
+        void Add(Person person);
+
+        void Update(Person person);
+
+        void Delete(Person person);
+
+        void Delete(int id);
+
+        Person GetById(int id);
+
+        ICollection<Person> GetAll();
+
+        ICollection<Person> Find(string firstName, string lastName);
+    }
+}
