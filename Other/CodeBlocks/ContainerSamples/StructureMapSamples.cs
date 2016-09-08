4f6dc3ecb7907ed6724c7267a2e0b1f7f6878aed
Mehdin Hodzic <mehdinh@itineris.ba>
2016-09-08 15:46:28 +0200

    Started to add DI container samples for document.

---
+++ Other/CodeBlocks/ContainerSamples/StructureMapSamples.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBlocks
+{
+    public class StructureMapSamples
+    {
+        public void Sample()
+        {
+            var container = new StructureMap.Container();
+            container.Configure(c => c.For<ILogger>().Use<ConsoleLogger>());
+            ILogger logger = container.GetInstance<ILogger>();
+        }
+        public void Sample2()
+        {             
+            var container = new StructureMap.Container(c =>
+            {
+                c.Scan(x =>
+                {
+                    x.TheCallingAssembly();
+                    x.WithDefaultConventions();
+                });
+            });
+        }
+    }
+}
